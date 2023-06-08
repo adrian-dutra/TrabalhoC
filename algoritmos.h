4abeb0d99fa0bd7eaea7abebe698a3d269a7d39f
adrian-dutra <117208341+adrian-dutra@users.noreply.github.com>
2023-06-08 18:11:39 -0400

    macaco macaco

--- algoritmos.h
+++ algoritmos.h
@@ -1,6 +1,9 @@
-
 void swap(int* a, int* b);
 void selectionSort(int n, int vet[]);
 void incertionSort(int n, int vet[]);
+void merge(int l, int m, int r, int vet[]);
+void mergeSort(int l, int r, int vet[]);
 void quickSort(int menor, int maior, int vet[]);
 int parte(int menor, int maior, int vet[]);
+int* criaVetorCrescente(int n);
+int* criaVetorDecrescente(int n);
--- main.c
+++ main.c
@@ -5,10 +5,10 @@ int main(){
        do
        {
             printf("===============menu===============\n");
-            printf("1 - opção\n");
-            printf("2 - opção\n");
-            printf("3 - opção\n");
-            printf("4 - opção\n");
+            printf("1 - Método de ordenação SelectionSort\n");
+            printf("2 - Método de ordenação InsertionSort\n");
+            printf("3 - Método de ordenação MergeSort\n");
+            printf("4 - Método de ordenação QuickSort\n");
             printf("0 - sair\n");
             printf("==================================\n");
             printf("escolha qual algoritmo quer rodar:\n");
@@ -17,13 +17,29 @@ int main(){
        switch (escolha)
        {
             case 1:
+            int* vetCrescente;
+            int tamanho = menuEscolhas();
+            vetCrescente = criaVetorCrescente(tamanho);
+            
+            for (int i = 0; i < tamanho; i++)
+            {
+               printf("%d", vetCrescente[i]);
+            }
+            
             break;
+
             case 2:
+            
             break;
+
             case 3:
+            
             break;
+
             case 4:
+            
             break;
+
             default:
             printf("Erro sua escolha deve ser entre 1 e 4 !");    
             break;
